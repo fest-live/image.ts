f0bcb5df9162a836c088f5d25ed5810f3ae51e17
L2NE-dev <conway24b@gmail.com>
2026-08-31 17:46:54 +0700

    Update Veela.CSS library...

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from "./engine/Config.js";
 export * from "./engine/KMean.js";
 export * from "./engine/WallpaperTheme.js";
 export * from "./canvas/Canvas.js";
-export * from "./canvas/Canvas-2.js";
+// WHY: Canvas-2.ts is a symlink to Canvas.ts. A second `export *` of the same
+// bindings makes Vite ESM fail: conflicting star export initializeAppCanvasLayer.
